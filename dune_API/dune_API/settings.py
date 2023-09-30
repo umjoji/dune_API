8471f833a24b70d9c59665af174472e977070145
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2023-09-30 22:23:22 +0100

    Remove unnecessary dotenv import in settings

--- dune_API/dune_API/settings.py
+++ dune_API/dune_API/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 import dj_database_url
 import os
 
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
